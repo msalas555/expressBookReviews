cc575688bbfe97542f2c1a1ae802077d4f385b87
msalas555 <salasmichael2004@gmail.com>
2024-09-04 13:25:30 -0400

    update auth_users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -46,12 +46,11 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //finish this ......
-  const username = req.session.username
-  const isbn = req.params.isbn
-  const review = req.body.review
+  const username = req.session.authorization["username"];
+  const isbn = req.params.isbn;
+  const review = req.query.review;
 
-  books[isbn]["reviews"][username] = review
+  books[isbn]["reviews"][username] = review;
   return res.status(200).json({"review added": books[isbn]});
 });
 
